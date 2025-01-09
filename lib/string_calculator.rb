3c6863160830fc6d54fd38dd6f555c2178d399f3
Abhay Chauhan <abhaychauhan@Abhays-MacBook-Air.local>
2025-01-09 11:43:18 +0530

    Used let in tests and added a dedicated class for string parsing. New tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,9 +1,18 @@
-require 'byebug'
-
 class StringCalculator
-	class << self
-		def add(numbers_string)
-			return 0 if numbers_string.empty?
-		end
-	end
+  class << self
+    def add(numbers_string)
+      numbers = StringParser.parse(numbers_string)
+      numbers.sum
+    end
+  end
+
+	private
+
+  class StringParser
+    def self.parse(numbers_string)
+      return [] if numbers_string.empty?
+
+      numbers_string.split(',').map(&:to_i)
+    end
+  end  
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,15 +1,29 @@
 require "string_calculator"
 
 RSpec.describe StringCalculator do
-	describe '#add' do
-		context "When empty string is passed" do
-			before :all do
-				@string_sum = StringCalculator.add("")
-			end
+  describe '#add' do
+    context "When empty string is passed" do
+      let(:string_sum) { StringCalculator.add("") }
 
-			it "Returns 0" do
-				expect(@string_sum).to eq(0)
-			end
-		end
-	end
+      it "Returns 0" do
+        expect(string_sum).to eq(0)
+      end
+    end
+
+    context "When single digit string is passed" do
+      let(:string_sum) { StringCalculator.add("10") }
+
+      it "Returns 10" do
+        expect(string_sum).to eq(10)
+      end
+    end
+
+    context "When multiple numbers are passed" do
+      let(:string_sum) { StringCalculator.add("1,2") }
+
+      it "Returns 3" do
+        expect(string_sum).to eq(3)
+      end
+    end
+  end
 end
